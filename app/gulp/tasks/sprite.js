e5e03fe4f704b58d9938cd8e18daba45e958f01d
TheeWebMaster <bavary1515@gmail.com>
2019-08-20 23:00:26 +0100

    cleaning sprites directories before creating new files.

--- app/gulp/tasks/sprite.js
+++ app/gulp/tasks/sprite.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
       rename = require('gulp-rename'),
-      svgSprite = require('gulp-svg-sprite');
+      svgSprite = require('gulp-svg-sprite'),
+      clean = require('gulp-clean');
 var config = {
   shape : {
     dimension : {
@@ -33,4 +34,8 @@ const moveSpriteSvg = () => {
   return gulp.src('./app/temp/sprite/css/**/*.svg')
              .pipe(gulp.dest('./app/assets/images/sprites/'));
 };
-gulp.task('sprite', gulp.series(createSprite, copySpriteCss, moveSpriteSvg));
+const deletePrevSvg = () => {
+  return gulp.src(['./app/temp/sprite/css','./app/assets/images/sprites'], {read : false, allowEmpty: true})
+             .pipe(clean())
+};
+gulp.task('createSprite', gulp.series(deletePrevSvg, createSprite, copySpriteCss, moveSpriteSvg));
