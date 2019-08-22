98f6e1d3b63bd99b260b3e294d2ebad4b988ed9f
TheeWebMaster <bavary1515@gmail.com>
2019-08-22 14:34:11 +0100

    overall fixes.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,22 +4,22 @@ import highlightLinksOnScroll from './modules/highlightLinksOnScroll';
 import $ from 'jquery';
 
 new RevealOnScroll($('.grid__item'), '75%', 'reveal-item');
-new RevealOnScroll($('.slide-right'), '80%', 'slide-right-on-scroll');
-new RevealOnScroll($('.slide-left'), '55%', 'slide-left-on-scroll');
+new RevealOnScroll($('.slide-right'), '60%', 'slide-right-on-scroll');
+new RevealOnScroll($('.slide-left'), '40%', 'slide-left-on-scroll');
 
-new CounterOnScroll($('#total-collections'), '90%', {
+new CounterOnScroll($('#total-collections'), '99%', {
   number :589393600,
   speed: 1233041
 });
-new CounterOnScroll($('#helped-people'), '90%', {
+new CounterOnScroll($('#helped-people'), '99%', {
   number :1393600,
   speed: 3041
 });
-new CounterOnScroll($('#total-volunteers'), '95%', {
+new CounterOnScroll($('#total-volunteers'), '99%', {
   number :304823,
   speed: 641
 });
-new CounterOnScroll($('#succeeded-missions'), '95%', {
+new CounterOnScroll($('#succeeded-missions'), '99%', {
   number :834329,
   speed: 1832
 });
